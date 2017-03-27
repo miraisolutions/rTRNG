429654bb1c6a9655b13b156135f3e36fa240280d
Riccardo Porreca <riccardo.porreca@mirai-solutions.com>
2017-03-27 23:01:35 +0200

    Use rTRNG in a package: "demo" review and update

--- inst/demos/rTRNGinPkg.R
+++ inst/demos/rTRNGinPkg.R
@@ -1,19 +1,31 @@
+
 demos <- "inst/demos"
 pkg <- "rTRNGinPkg"
 pkgDir <- file.path(tempdir(), pkg)
 
+# cleanup
+on.exit({
+  unlink(pkgDir, recursive = TRUE)
+  remove.packages(pkg)
+})
+
+
 Rcpp::Rcpp.package.skeleton(
   pkg, path = dirname(pkgDir),
   cpp_files = file.path(demos, "skeletonFiles",
-                        c("Makevars", "Makevars.win",
+                        c(# linker flags via rTRNG::LdFlags()
+                          "Makevars", "Makevars.win",
+                          # simple Rcpp function using TRNG
                           "testUsingrTRNG.cpp")))
 
+# Imports and LinkingTo
 devtools::use_package(pkg = pkgDir, package = "rTRNG", type = "Imports")
 devtools::use_package(pkg = pkgDir, package = "rTRNG", type = "LinkingTo")
-devtools::install(pkg = pkgDir, quick = TRUE, quiet = TRUE)
+
+# import one symbol in the package NAMESPACE to ensure correct loading
+writeLines("importFrom(rTRNG, TRNG.Version)",
+           file(file.path(pkgDir, "NAMESPACE"), "a"))
+
+devtools::install(pkg = pkgDir)
 
 rTRNGinPkg::testUsingrTRNG(10)
-
-# cleanup
-unlink(pkgDir, recursive = TRUE)
-remove.packages(pkg)
