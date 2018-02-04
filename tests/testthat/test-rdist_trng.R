c8143733661be67f02b969202e639bcfdd75395e
Riccardo Porreca <riccardo.porreca@mirai-solutions.com>
2018-02-04 20:21:42 +0100

    Add tests for r<dist>_trng. Generic and extensible, tests for the existing runif_trng, rnorm_trng

---
+++ tests/testthat/test-rdist_trng.R
@@ -0,0 +1,49 @@
+KIND <- "yarn4"
+KIND_CLASS <- getFromNamespace(KIND, "rTRNG")
+SEED <- 117L
+SAMPLES <- 15L
+SAMPLES_PAR <- 1000L
+
+
+test_rdist <- function(rdist, ...) {
+
+  rdist_trng <- sprintf("%s_trng", rdist)
+  context(rdist_trng)
+  rdist_fun <- getFromNamespace(rdist_trng, "rTRNG")
+
+  test_that(paste(rdist_trng, "returns the correct number of samples"), {
+    TRNGkind(KIND)
+    TRNGseed(SEED)
+    expect_identical(length(rdist_fun(SAMPLES, ...)),
+                     SAMPLES)
+  })
+
+  test_that(paste(rdist_trng, "works with explicit 'engine' argument"), {
+    TRNGkind(KIND)
+    TRNGseed(SEED)
+    e <- KIND_CLASS$new(SEED)
+    expect_identical(rdist_fun(SAMPLES, ...),
+                     rdist_fun(SAMPLES, ..., engine = e))
+  })
+
+  test_that(paste(rdist_trng, "parallel genration via 'parallelGrain' works"), {
+    TRNGkind(KIND)
+    TRNGseed(SEED)
+    x_serial <- rdist_fun(SAMPLES_PAR, ...)
+    TRNGseed(SEED)
+    x_parallel <- rdist_fun(SAMPLES_PAR, ..., parallelGrain = 50L)
+    expect_identical(x_serial, x_parallel,
+                     info = "TRNG.Random current engine")
+    e <- KIND_CLASS$new(SEED)
+    x_serial <- rdist_fun(SAMPLES_PAR, ..., engine = e)
+    e$seed(SEED)
+    x_parallel <- rdist_fun(SAMPLES_PAR, ..., engine = e, parallelGrain = 50L)
+    expect_identical(x_serial, x_parallel,
+                     info = "explicit engine")
+  })
+
+}
+
+
+test_rdist("runif")
+test_rdist("rnorm")
